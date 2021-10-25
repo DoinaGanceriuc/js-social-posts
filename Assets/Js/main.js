684491e4acf16de21a7fe6a3867db0c9f05fb1b8
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-25 15:38:05 +0200

    aggiornate immagini profili

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -10,9 +10,9 @@ const posts = [
     {
         id: 1,
         contenuto: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, modi aliquid, ipsa voluptatum optio nihil molestiae in dolore dolores, quis omnis libero qui eaque itaque deserunt! Quisquam, at quibusdam! Cum.",
-        immagine: "https://unsplash.it/300/300?image",
+        immagine: "https://i.picsum.photos/id/864/300/300.jpg?hmac=vS7RjFd0kISq4skaH_DLn2J_I5p5i-Z3w5Eob_LwlAk",
         nomeAutore: "Phil Mangione",
-        avatarAutore: "https://unsplash.it/300/300?image",
+        avatarAutore: "https://i.picsum.photos/id/103/300/300.jpg?hmac=g2qp6CaucrgdrPEmPsf-cpy7hrnv6UDL3epElUsnMyQ",
         numeroLikes: 80,
         dataCreazione:"4 mesi fa"
 
@@ -20,9 +20,9 @@ const posts = [
     {
         id: 2,
         contenuto: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, modi aliquid, ipsa voluptatum optio nihil molestiae in dolore dolores, quis omnis libero qui eaque itaque deserunt! Quisquam, at quibusdam! Cum.",
-        immagine: "https://unsplash.it/300/300?image",
+        immagine: "https://i.picsum.photos/id/184/300/300.jpg?hmac=vhW7YQcLmc_QysIuIuS8uSu6lbXS4Dw9btPbw2cQn90",
         nomeAutore: "Sofia Perlari",
-        avatarAutore: "https://unsplash.it/300/300?image",
+        avatarAutore: "https://i.picsum.photos/id/628/300/300.jpg?hmac=ML4RdmepzVTOl4zptucXgnlwbQ8mSbJ3TiL5U5fgGvQ",
         numeroLikes: 75,
         dataCreazione:"2 mesi fa"
 
@@ -30,9 +30,9 @@ const posts = [
      {
         id: 3,
         contenuto: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, modi aliquid, ipsa voluptatum optio nihil molestiae in dolore dolores, quis omnis libero qui eaque itaque deserunt! Quisquam, at quibusdam! Cum.",
-        immagine: "https://unsplash.it/300/300?image",
+        immagine: "https://i.picsum.photos/id/120/300/300.jpg?hmac=hDgY6Ddzn2TXPA1WBM44RB5mka-jShgV7sXprLSVH4Y",
         nomeAutore: "Mario Rossi",
-        avatarAutore: "https://unsplash.it/300/300?image",
+        avatarAutore: "https://i.picsum.photos/id/845/300/300.jpg?hmac=q5XPOzz2vTZt_PpOgSI8u8tw7bnG85xz42PjWLOcJ6U",
         numeroLikes: 0,
         dataCreazione:"1 mese fa"
 
@@ -40,9 +40,9 @@ const posts = [
      {
         id: 4,
         contenuto: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, modi aliquid, ipsa voluptatum optio nihil molestiae in dolore dolores, quis omnis libero qui eaque itaque deserunt! Quisquam, at quibusdam! Cum.",
-        immagine: "https://unsplash.it/300/300?image",
+        immagine: "https://i.picsum.photos/id/179/300/300.jpg?hmac=EG56WiFo72qraubmhi9W4fHqaomaMxQ3gMwo6pKvVNs",
         nomeAutore: "Federica Bianchi",
-        avatarAutore: "https://unsplash.it/300/300?image",
+        avatarAutore: "https://i.picsum.photos/id/464/300/300.jpg?hmac=PvV-hZNabWHmZw8hesE0G3qvVAv1JoAsO_Gl4tqYJOY",
         numeroLikes: 0,
         dataCreazione:"3 mesi fa"
 
@@ -50,11 +50,13 @@ const posts = [
 
 ]
 
-const likes = [3, 4]
+const likes = [1, 2]
 
 for (let index = 0; index < posts.length; index++) {
     const singlePost = posts[index];
     console.log(singlePost);
+    const profileImg = singlePost.avatarAutore;
+    console.log(profileImg);
     const postElement = `<div class="cards">
                 <div class="profile">
                     <img id="profile_img"
